c6a087efb60991e88686e8ddb1854f8c99556668
jackletter <1286317554@qq.com>
2017-01-08 20:59:58 +0800

    增加sqlite支持,增加sqlserver表管理

---
+++ Result.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DBUtil
+{
+    public class Result
+    {
+        public bool Success { set; get; }
+        public object Data { set; get; }
+    }
+}
---
+++ TableStruct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DBUtil
+{
+    public class TableStruct
+    {
+        public string Name { set; get; }
+        public string Desc { set; get; }
+        public string PrimaryKey { set; get; }
+        public List<Column> Columns = new List<Column>();
+        public class Column
+        {
+            public string Name { set; get; }
+            public string Type { set; get; }
+            public string Desc { set; get; }
+
+            public bool IsIdentity { set; get; }
+
+            public bool IsNullable { set; get; }
+
+            public bool IsPrimaryKey { set; get; }
+
+            public string Default { set; get; }
+
+            public int MaxLength { set; get; }
+
+            public int Start { set; get; }
+
+            public int Incre { set; get; }
+
+            public bool IsUnique { set; get; }
+        }
+    }
+}
